367938d28c4be0bc4748c280225dc0f199e6f8aa
Junhwan Kim <hwan@linuxgeek.io>
2023-01-02 21:22:23 +0900

    django settings import: different way

--- send_mail_app/tasks.py
+++ send_mail_app/tasks.py
@@ -2,7 +2,8 @@ from django.contrib.auth import get_user_model
 
 from celery import shared_task
 from django.core.mail import send_mail
-from django_celery_project import settings
+# from django_celery_project import settings
+from django.conf import settings
 
 @shared_task(bind=True)
 def send_mail_func(self):
